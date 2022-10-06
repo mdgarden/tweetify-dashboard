8a9469267a6483bea72366dcef054577d008ad7a
Madang <chooi9522@gmail.com>
2022-10-06 23:51:36 +0900

    feature : get code from login user

--- client/src/components/SpotifyCallback/index.tsx
+++ client/src/components/SpotifyCallback/index.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import axios from 'axios';
+import { Buffer } from 'buffer';
+import { response } from 'express';
+import qs from 'qs';
 
 const SpotifyCallback = () => {
+  const [searchParams] = useSearchParams();
+  const code = searchParams.get('code');
+  const client = axios.create({
+    baseURL: 'hhttps://accounts.spotify.com/api/token',
+    headers: {
+      Authorization:
+        'Basic ' +
+        Buffer.from(
+          process.env.REACT_APP_SPOTIFY_CLIENT_ID +
+            ':' +
+            process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
+        ).toString('base64'),
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        params: qs.stringify({
+          code: code,
+          redirect_uri: process.env.REACT_APP_SPOTIFY_REDIRECT_URI,
+          grant_type: 'authorization_code',
+        }),
+        headers: {
+          Authorization:
+            'Basic ' +
+            Buffer.from(
+              process.env.REACT_APP_SPOTIFY_CLIENT_ID +
+                ':' +
+                process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
+            ).toString('base64'),
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
+      if (response.data.accessCode === 200) {
+        console.log(response.data.accessCode);
+      }
+    })();
+  }, []);
   return (
     <>
       <div className="callback">Callback</div>
+      <div>{code}</div>
     </>
   );
 };
--- client/src/components/SpotifyLogin/index.tsx
+++ client/src/components/SpotifyLogin/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SpotifyLogin = () => {
   const scope =
     'user-read-email user-read-private user-read-playback-state user-top-read';
-  const authURL = `https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&scpoe=${scope}&redirct_uri='http://localhost:3000/auth/spotify/callback'`;
+  const authURL = `https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&scpoe=${scope}&redirect_uri=http://localhost:3000/auth/spotify/callback`;
 
   return (
     <>
